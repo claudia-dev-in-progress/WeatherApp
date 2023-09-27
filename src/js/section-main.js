82bd7c952077a7088e8810df6b04ea293cd5a0cb
claudiapapp <122099742+claudiapapp@users.noreply.github.com>
2023-09-27 19:35:31 +0000

    Deploying to gh-pages from @ claudiapapp/WeatherApp@5247f66995197730c02621b4d4193e356aee7294 🚀

--- src/js/section-main.js
+++
@@ -1,8 +0,0 @@
-import { getCityImage } from './pixabay-api';
-
-export async function updateCityImage(city) {
-  const imageUrl = await getCityImage(city);
-  const containerBackground = document.querySelector('.container__background');
-
-  containerBackground.style.backgroundImage = `url(${imageUrl})`;
-}
